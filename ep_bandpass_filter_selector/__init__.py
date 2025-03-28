465d46ccecf65b784a153ffb166d0304cd634906
Yaroslav Pisarev <yaricp@gmail.com>
2025-03-28 18:46:45 +0100

    added __init__

--- ep_bandpass_filter_selector/__init__.py
+++ ep_bandpass_filter_selector/__init__.py
@@ -2,5 +2,5 @@ from .selector import PassbandSelector, export_data
 
 
 __all__ = ["PassbandSelector", "export_data"]
-__version__ = "0.0.1"
-__version_info__ = (0, 0, 1)
+__version__ = "0.2.1"
+__version_info__ = (0, 2, 1)
